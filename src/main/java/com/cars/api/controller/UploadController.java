275653a9b4c104ba07bdc616a409df69cdb735f3
Marcelo Amorim <mamorim813@gmail.com>
2024-01-10 18:32:25 -0300

    Update Swagger to pass through Fix '/' missing in the @RequestMapping of UploadController

--- src/main/java/com/cars/api/controller/UploadController.java
+++ src/main/java/com/cars/api/controller/UploadController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.io.IOException;
 
 @RestController
-@RequestMapping("api/v1/upload")
+@RequestMapping("/api/v1/upload")
 public class UploadController {
 
     @Autowired
